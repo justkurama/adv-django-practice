6667cc29819982edab545b09514fd00c14fc954b
Kurmanbek Tolebayev <tolebayevk0723@gmail.com>
2025-02-14 22:16:18 +0500

    prc1 changed

--- practice-2/cv/cv/settings.py
+++ practice-2/cv/cv/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -62,7 +63,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'zhaslanbeksultan@gmail.com'
+EMAIL_HOST_USER = 'tolkurama@gmail.com'
 
 EMAIL_HOST_PASSWORD = ''
 
